c0f6fbf39aada8a9f792b8ac2319ca5de90b67a5
vsc2695 <vsc2695@gmail.com>
2025-05-06 21:49:29 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 
 import Hero from "./components/Hero.jsx"
 import Content from "./components/Content.jsx"
+import Stats from './components/Stats';
+import CTA from './components/CTA';
+import Testimonials from "./components/Testimonials.jsx";
 
 function App() {
   
   return (
    <>
     <Hero/>
+    <Stats/>
     <Content/>
+    <CTA/>
+    <Testimonials/>
    </>
    )
 }
